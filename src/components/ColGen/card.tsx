48d8b81c80a9c18d280d8a7cff2924ed03d62a05
Amit Singh <amit195@hotmail.com>
2024-02-14 12:22:34 +0530

    added utility to generate hex code directly from fn

--- src/components/ColGen/card.tsx
+++ src/components/ColGen/card.tsx
@@ -1,18 +1,10 @@
-import { getRGB } from '../../utils';
+import { getHexColor } from '../../utils';
 type Props = {
   color: string;
 };
 
 export default function Card({ color }: Props) {
-  const rgb = getRGB(color);
-  if (!rgb) return null;
-  const rHex = parseInt(rgb[0]).toString(16);
-  const gHex = parseInt(rgb[1]).toString(16);
-  const bHex = parseInt(rgb[1]).toString(16);
-
-  let hexCode = `${rHex}${gHex}${bHex}`;
-  hexCode = hexCode.padStart(6, '0');
-  hexCode = `#${hexCode}`.toUpperCase();
+  let hexCode = getHexColor(color) ?? '#000';
   return (
     <div
       className="flex flex-col items-center justify-end"
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,40 @@
-export function generateRandomColor() {
+export function generateRandomColor(hex: boolean = false): string {
   const min = 0;
   const max = 255;
   const r = Math.floor(Math.random() * max);
   const g = Math.floor(Math.random() * max);
   const b = Math.floor(Math.random() * max);
-  return `rgb(${r},${g},${b})`;
+  let hexCode = '#';
+
+  if (hex) {
+    const rHex = r.toString(16);
+    const gHex = r.toString(16);
+    const bHex = r.toString(16);
+    hexCode = `${rHex}${gHex}${bHex}`.padStart(6, '0');
+    hexCode = `#${hexCode}`.toUpperCase();
+  }
+  return hex ? hexCode : `rgb(${r},${g},${b})`;
 }
 
-export function generatePalette(size = 5) {
+export function generatePalette(size = 5): Array<string> {
   return new Array(size).fill(null).map(generateRandomColor);
 }
 
-export function getRGB(color: string) {
+export function parseRGB(color: string): Array<string> | null {
   const regex = /\((.*?)\)/g;
   const match: RegExpExecArray | null = regex.exec(color);
-  return match ? match[1].split(',') : null;
+  if (!match) return null;
+  return match[1].split(',');
+}
+
+export function getHexColor(rgbColor: string): string | null {
+  const rgb = parseRGB(rgbColor);
+  if (!rgb) return null;
+  const rHex = parseInt(rgb[0]).toString(16);
+  const gHex = parseInt(rgb[1]).toString(16);
+  const bHex = parseInt(rgb[1]).toString(16);
+  let hexCode = `${rHex}${gHex}${bHex}`;
+  hexCode = hexCode.padStart(6, '0');
+  hexCode = `#${hexCode}`.toUpperCase();
+  return hexCode;
 }
