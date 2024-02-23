ccd040dc3cdc0a49931031e74fb0e6f2cb5e6b10
Amit Singh <amit195@hotmail.com>
2024-02-23 16:08:21 +0530

    QRCode Generator

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,7 +1,12 @@
-type props = {
-  url: string;
-};
-export default function QrCode({ url }: props) {
-  console.log(url);
-  return <div>QR Code Generator</div>;
+export default function QrCode({ data = "Hello World" }: { data: string }) {
+  return (
+    <div className="flex h-[356px] w-full items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <img
+        width={256}
+        height={256}
+        className="rounded-xl"
+        src={`https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=${encodeURIComponent(data)}&choe=UTF-8`}
+      />
+    </div>
+  );
 }
--- src/pages/qr-code.tsx
+++ src/pages/qr-code.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import QrCode from "../components/QrCode";
 export default function Page() {
+  const [data, setData] = useState("");
   return (
-    <div>
-      <QrCode url={""} />
+    <div className="flex h-screen w-full items-center justify-center bg-slate-50">
+      <div className="flex w-[350px] flex-col items-center overflow-hidden rounded-lg bg-white shadow-lg">
+        <QrCode data={data} />
+        <input
+          id="data"
+          name="data"
+          type="text"
+          required
+          onChange={(e) => setData(e.target.value)}
+          placeholder="enter text/url to generate QRCode"
+          className="my-4 block w-[95%] rounded-md border border-slate-300 px-2 py-1.5 text-gray-900 shadow-sm outline-none focus-visible:border-blue-500 sm:text-sm sm:leading-6"
+        />
+      </div>
     </div>
   );
 }
